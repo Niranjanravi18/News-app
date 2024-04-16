e9773803cd26452a9b9a3639866cd67eab3c59a4
Niranjan <155443846+Niranjanravi18@users.noreply.github.com>
2024-04-17 01:39:00 +0530

    Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Navbar = ({setCategory}) => {
   return (
     <div>
-        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light" > */}
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="#"><span className='badge bg-light text-dark fs-4'>NewsMag</span></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
